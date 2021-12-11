755b39f6852bff6fb85a6fe7ad06848bad970aa9
Aayush Basnet <aayushbasnet07@gmail.com>
2021-12-11 18:28:05 +0545

    replaced callbacks with promises

--- quote.js
+++ quote.js
@@ -3,43 +3,44 @@ const gBtn = document.querySelector('.gBtn');
 const author = document.querySelector('.author');
 const quote = document.querySelector('.quote');
 
-const randInt = (min, max) =>{
-    return (Math.floor(Math.random() * (max-min))+ min)
+//generate random interger between numbers
+const randInt = (min, max) => {
+    return (Math.floor(Math.random() * (max - min)) + min)
 };
 
-const quoteGen = (callback) =>{
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () =>{
-        if(request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText);
-            // console.log(data);
-            callback(undefined, data);
-        }else if (request.readyState === 4){
-            // console.log('Cannot fetch the datas');
-            callback('Cannot fetch the datas', undefined);
-        }
-    });
-    request.open('GET', 'https://type.fit/api/quotes');
-    // request.open('GET', 'quote.json');
-    request.send();
-};
-
-gBtn.addEventListener('click', e =>{
-    quoteGen((err, data) =>{
-        if(err){
-            console.log('Cannot fetch the data');
-        }else{
-            const newInt = randInt(0, data.length);
-            var prevInt = -1;
-            if(newInt != prevInt){
-                quote.textContent = `" ${data[newInt].text} "`;
-                author.textContent = `- ${data[newInt].author}`;
-                prevInt = newInt;
-                console.log('same value', newInt, prevInt);
-            }else{
-                console.log('same value', newInt, prevInt);
+//request data from api (promise used)
+const quoteGen = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                // callback(undefined, data);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                // callback('Cannot fetch the datas', undefined);
+                reject('Cannot fetch the resources');
             }
+        });
+        request.open('GET', resource);
+        // request.open('GET', 'quote.json');
+        request.send();
+    });
+};
+
+gBtn.addEventListener('click', () => {
+    quoteGen('https://type.fit/api/quotes').then((data) => {
+        const newInt = randInt(0, data.length);
+        var prevInt = -1;
+        if (newInt != prevInt) {
+            quote.textContent = `" ${data[newInt].text} "`;
+            author.textContent = `- ${data[newInt].author}`;
+            // prevInt = newInt;
+            // console.log('same value', newInt, prevInt);
+        } else {
+            console.log('same value', newInt, prevInt);
         }
+    }).catch((err) =>{
+        console.log('promise rejected: ', err);
     });
 });
